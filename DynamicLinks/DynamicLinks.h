f6bb18eb5a11e93e7a1b90625973a50f5b627988
Nicolas Landa <nicolas.landa@ara-tech.es>
2017-10-20 15:00:39 +0200

    Initial Commit

---
+++ DynamicLinks/DynamicLinks.h
@@ -0,0 +1,19 @@
+//
+//  DynamicLinks.h
+//  DynamicLinks
+//
+//  Created by Aratech iOS on 20/10/17.
+//  Copyright © 2017 machukas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicLinks.
+FOUNDATION_EXPORT double DynamicLinksVersionNumber;
+
+//! Project version string for DynamicLinks.
+FOUNDATION_EXPORT const unsigned char DynamicLinksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicLinks/PublicHeader.h>
+
+
